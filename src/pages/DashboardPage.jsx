3d1355141a4114b3a0ea5a7a00c8058868e2325c
Muhammad Anas <anas.dev122@gmail.com>
2026-07-09 18:01:34 +0400

    now calling this API

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import PageHeader from "../components/dashboard/PageHeader";
 import DashboardCharts from "../components/dashboard/DashboardCharts";
+import axios from "axios";
 
 const DashboardPage = () => {
+  const getData = async (url) => {
+    try {
+      const data = await axios.get(`http://localhost:5000/trend`);
+      console.log(data?.data?.data);
+    } catch (error) {
+      console.error(error.message);
+      throw error;
+    }
+  };
+
+  getData();
+
   return (
     <div className="space-y-6">
       <PageHeader />
